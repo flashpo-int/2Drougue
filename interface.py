fbd528133bf2173ad3491787c6fd3464ddd300c8
xwinp <139521361+xwinp@users.noreply.github.com>
2023-10-13 17:33:08 +0800

    Add files via upload

--- interface.py
+++ interface.py
@@ -5,12 +5,13 @@ class Gamestatus():
     def __init__(self,screen,ai_settings) -> None:
         self.ai_settings=ai_settings
         self.screen=screen
+        self.person_id=0
         self.reset_stats()
     def reset_stats(self):
         # self.ship_left=self.ai_settings.ship_limit
         self.game_start=0
         self.pause=0
-        # self.easy=1
+        self.easy=1
         self.page=0
         self.score=0
         # self.reward=0
@@ -28,6 +29,14 @@ class Gamestatus():
         elif event.type==pygame.KEYDOWN:
             if event.key==pygame.K_ESCAPE:
                 self.pause^=1
+    def create_person(self,centerx,name):
+        b=self.create_button("",150,400,0,True,(255,255,0))
+        b.rect.center=(centerx,300)
+        b.draw_button()
+        b_name=self.create_button(name,150,50,48,True,(0,255,0))
+        b_name.rect.center=(centerx,600)
+        b_name.draw_button()
+        return b
     def draw_page(self):
         if self.page==0:
             # pass
@@ -44,46 +53,82 @@ class Gamestatus():
             self.quit_button=self.new_button("quit",100,450)
         elif self.page==1:
             self.back_button=self.new_button("Back",100,50)
+            self.p1_button=self.create_person(200,"p1")
+            self.p2_button=self.create_person(600,"p2")
+            self.p3_button=self.create_person(1000,"p3")
+            
         elif self.page==2:
             self.back_button=self.new_button("Back",100,50)
         elif self.page==3:
             self.back_button=self.new_button("Back",100,50)
         elif self.page==4:
             self.back_button=self.new_button("Back",100,50)
+        elif self.page==11:
+            self.back_button=self.new_button("Back",100,50)
+            self.easy_button=self.new_button("easy",400,400)
+            self.hard_button=self.new_button("hard",800,400)
+            
+            
     def show_other(self):
         if self.pause==1:
-            self.small_screen=self.create_button("a",self.ai_settings.small_screen_width,self.ai_settings.small_screen_height,0,True,color=(255,255,255))
+            self.small_screen=self.create_button("",self.ai_settings.small_screen_width,self.ai_settings.small_screen_height,0,True,color=(255,255,255))
             self.small_screen.rect.center=(self.ai_settings.screen_width/2,self.ai_settings.screen_height/2)
             self.small_screen.draw_button()
+            self.menu_button=self.new_button("Menu",600,600)
+    def colli(self,bt):
+        return bt.rect.collidepoint(self.mouse_x,self.mouse_y)
     def turn_page(self,mouse_x,mouse_y):
+        self.mouse_x=mouse_x
+        self.mouse_y=mouse_y
         if self.pause==1:
+            if self.colli(self.menu_button):
+                self.game_start=0
+                self.page=0
+                self.pause=0
             return
         if self.page==0:
-            if self.start_button.rect.collidepoint(mouse_x,mouse_y):
+            if self.colli(self.start_button):
                 self.page=1
-                self.game_start=1
-            elif self.talent_button.rect.collidepoint(mouse_x,mouse_y):
+            elif self.colli(self.talent_button):
                 self.page=2
-            elif self.config_button.rect.collidepoint(mouse_x,mouse_y):
+            elif self.colli(self.config_button):
                 self.page=3
-            elif self.save_button.rect.collidepoint(mouse_x,mouse_y):
+            elif self.colli(self.save_button):
                 self.page=4
-            if self.quit_button.rect.collidepoint(mouse_x,mouse_y):
+            if self.colli(self.quit_button):
                 sys.exit()
         elif self.page==1:
-            if self.back_button.rect.collidepoint(mouse_x,mouse_y):
+            if self.colli(self.back_button):
                 self.page=0
+            elif self.colli(self.p1_button):
+                self.page=11
+                self.person_id=1
+            elif self.colli(self.p2_button):
+                self.page=11
+                self.person_id=2
+            elif self.colli(self.p3_button):
+                self.page=11    
+                self.person_id=3
         elif self.page==2:
-            if self.back_button.rect.collidepoint(mouse_x,mouse_y):
+            if self.colli(self.back_button):
                 self.page=0
         elif self.page==3:
-            if self.back_button.rect.collidepoint(mouse_x,mouse_y):
+            if self.colli(self.back_button):
                 self.page=0
         elif self.page==4:
-            if self.back_button.rect.collidepoint(mouse_x,mouse_y):
+            if self.colli(self.back_button):
                 self.page=0
-            
-            
+        elif self.page==11:
+            if self.colli(self.back_button):
+                self.page=1
+            if self.colli(self.easy_button):
+                self.easy=1
+                self.page=111
+                self.game_start=1
+            if self.colli(self.hard_button):
+                self.easy=0
+                self.page=111
+                self.game_start=1
             
         # elif self.page==1:
         #     if self.easy_rect.collidepoint(mouse_x,mouse_y):
