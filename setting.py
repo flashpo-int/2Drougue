a61640d19ad9b56896ad9d24c8729f5390e671e4
xwinp <139521361+xwinp@users.noreply.github.com>
2023-10-13 16:30:34 +0800

    Add files via upload

--- setting.py
+++ setting.py
@@ -1,5 +1,8 @@
 class Settings():
     def __init__(self):
         self.screen_width = 1200 
-        self.screen_height = 800 
+        self.screen_height = 800
+        self.small_screen_width= 1000
+        self.small_screen_height=600 
         self.bg_color = (230, 230, 230)
+        self.small_bg_color=(100,100,100)
